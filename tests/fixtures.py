054a8bc20401fff935bb5b50c8081200d639ae0f
Andrew <andrew.vk90@gmail.com>
2023-05-28 13:43:52 +0300

    tested completed (selection+)

--- tests/fixtures.py
+++ tests/fixtures.py
@@ -12,3 +12,14 @@ def access_token(client, django_user_model):
     django_user_model.objects.create(username=username, password=password)
     response = client.post("/user/token/", data = {"username": username, "password": password})
     return response.data.get("access")
+
+
+@pytest.fixture
+@pytest.mark.django_db
+def user_with_access_token(client, django_user_model):
+    #test_user = UserFactory.create()
+    username = "test"
+    password = "qwe123"
+    user = django_user_model.objects.create(username=username, password=password)
+    response = client.post("/user/token/", data = {"username": username, "password": password})
+    return user, response.data.get("access")
